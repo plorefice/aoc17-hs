d2bedaf2cc83f9448c7e2fbdd1879d85f6e22240
Pietro Lorefice <pietro.lorefice@gmail.com>
2017-12-18 20:01:32 +0100

    Task 7: second subtask implementation

--- task7/app/Main.hs
+++ task7/app/Main.hs
@@ -4,6 +4,7 @@ import Lib
 
 import Control.Monad
 import Data.List
+import qualified Data.Map as M
 
 data Tree = Nil | Tree Proc [Tree] deriving (Show, Eq)
 
@@ -12,6 +13,7 @@ main = do
     input <- getContents
     let procs = procList . lines $ input
     putStrLn . ("7a: " ++) . root . build $ procs
+    putStrLn . ("7b: " ++) . show . adjustedWeight . build $ procs
 
 procList :: [String] -> [Proc]
 procList = map parseProc
@@ -43,3 +45,22 @@ insert' p@(Proc pn _ pc) t@(Tree p'@(Proc tn _ tc) tl)
         in  case attempt of
                 (Nothing, _)   -> Left t
                 (Just idx, t') -> Right $ Tree p' (take idx tl ++ (t' : (drop (idx + 1) tl)))
+
+adjustedWeight :: Tree -> Int
+adjustedWeight t =
+    let ((Tree _ children), target) = findUnbalanced t 0
+        childrenWeight = sum . map weight $ children
+    in  target - childrenWeight
+
+weight :: Tree -> Int
+weight (Tree (Proc _ w _) tl) = w + (sum . map weight $ tl)
+
+findUnbalanced :: Tree -> Int -> (Tree, Int)
+findUnbalanced t@(Tree p@(Proc _ w _) tl) target =
+    let weights    = map weight tl
+        indexed    = M.fromListWith (++) . zip weights . map (:[]) $ [0..]
+    in  if length indexed == 1
+        then (t, target)
+        else let unbalanced = tl !! (head . snd . M.elemAt 0 . M.filter (\l -> length l == 1) $ indexed)
+                 target'    = head . M.keys . M.filter (\l -> length l > 1) $ indexed
+             in  findUnbalanced unbalanced target'
