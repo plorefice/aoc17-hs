44dc641a5688acd49b86e5125fb7b56afe071794
Pietro Lorefice <pietro.lorefice@gmail.com>
2017-12-19 16:58:47 +0100

    Task 11: second subtask implementation

--- task11/app/Main.hs
+++ task11/app/Main.hs
@@ -10,17 +10,21 @@ main :: IO ()
 main = do
     input <- getContents
     let dirs = map parseDir . splitOn "," $ input
-        dest = foldl (flip step) (0,0,0) dirs 
+        (dest, maxDist) = foldl (flip step) ((0,0,0), 0) dirs 
     putStrLn . ("11a: " ++) . show . distance (0,0,0) $ dest
+    putStrLn . ("11a: " ++) . show $ maxDist
     
-step :: Direction -> Cell -> Cell
-step dir (x, y, z) = case dir of
-    N  -> (x+1, y-1, z)
-    S  -> (x-1, y+1, z)
-    NW -> (x+1, y, z-1)
-    NE -> (x, y-1, z+1)
-    SW -> (x, y+1, z-1)
-    SE -> (x-1, y, z+1)
+step :: Direction -> (Cell, Int) -> (Cell, Int)
+step dir ((x, y, z), dist) = 
+    let c' = case dir of
+                N  -> (x+1, y-1, z)
+                S  -> (x-1, y+1, z)
+                NW -> (x+1, y, z-1)
+                NE -> (x, y-1, z+1)
+                SW -> (x, y+1, z-1)
+                SE -> (x-1, y, z+1)
+        dist' = distance (0,0,0) c'
+    in  (c', max dist dist')
 
 distance :: Cell -> Cell -> Int
 distance (x,y,z) (x',y',z') = (abs (x - x') + abs (y - y') + abs (z - z')) `div` 2
