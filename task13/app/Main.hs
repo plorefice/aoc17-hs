24ffb6bb492ab5f6b2e8c36fe192cfef54f71094
Pietro Lorefice <pietro.lorefice@gmail.com>
2017-12-21 18:14:55 +0100

    Task 13: second subtask implementation

--- task13/app/Main.hs
+++ task13/app/Main.hs
@@ -1,46 +1,64 @@
 module Main where
 
+import Data.Monoid
 import qualified Data.Map as M
 import qualified Data.List.Split as S
 
-type Level = (Int, Int)
-type Firewall = M.Map Int Int
-data Dir = Down | Up deriving (Show, Eq)
-type State = (Int, M.Map Int (Dir, Int))
+type Position  = Int
+type Severity  = Int
+type Level     = (Int, Int)
+type Firewall  = M.Map Int Int
+data Direction = Down | Up deriving (Show, Eq)
+type Detection = (Bool, Severity)
+type Scanners  = M.Map Int (Direction, Int)
+type State     = (Position, Detection, Scanners)
 
 main :: IO ()
 main = do
     input <- getContents
     let fw      = M.fromList . map parseLevel . lines $ input
-        lastLvl = fst . M.findMax $ fw
-        initial = (-1, M.map (\_ -> (Down, 0)) fw)
-        trip'   = trip fw initial lastLvl
-    putStrLn . ("13a: " ++) . show . sum . map fst $ trip'
+        initial = (-1, undetected, M.map (\_ -> (Down, 0)) fw)
+        steps   = trip fw initial
+    putStrLn . ("13a: " ++) . show . sum . map (\(_, (_, sev), _) -> sev) $ steps
+    putStrLn . ("13b: " ++) . show . findDelay fw $ initial
 
 parseLevel :: String -> Level
 parseLevel s = (read $ toks !! 0, read $ toks !! 1)
     where toks = S.splitOn ": " s
 
-advance :: Firewall -> State -> State
-advance fw (pos, st) = (pos + 1, M.mapWithKey f st)
-    where f lvl cur@(dir, scanner) = advance' (fw M.! lvl) cur
+undetected :: Detection
+undetected = (False, 0)
 
-advance' :: Int -> (Dir, Int) -> (Dir, Int)
-advance' max (dir, cur)
+detect :: Firewall -> State -> Detection
+detect fw (pos, _, scanners) = case fw M.!? pos of
+    Nothing    -> undetected
+    Just range -> if (snd . (M.!) scanners $ pos) /= 0 then (False, 0) else (True, pos * range)
+
+step :: Firewall -> State -> State
+step fw st@(pos, _, scanners) = (pos', detection, scanners')
+    where pos'      = pos + 1
+          detection = detect fw (pos', undetected, scanners)
+          scanners' = M.mapWithKey (\lvl s -> advance (fw M.! lvl) s) scanners
+
+advance :: Int -> (Direction, Int) -> (Direction, Int)
+advance max (dir, cur)
     | dir == Down = (if cur == (max - 2) then Up else Down, cur + 1)
     | dir == Up   = (if cur == 1 then Down else Up, cur - 1)
 
-severity :: Firewall -> State -> Int
-severity fw (pos, st) = case fw M.!? pos of
-    Nothing    -> 0
-    Just range -> if (snd . (M.!) st $ pos) /= 0 then 0 else pos * range
+trip :: Firewall -> State -> [State]
+trip fw st = trip' fw st (fst . M.findMax $ fw)
 
-step :: Firewall -> State -> (Int, State)
-step fw s@(pos, st) = (sev, next)
-    where sev  = severity fw (pos + 1, st)
-          next = advance fw s
+trip' :: Firewall -> State -> Int -> [State]
+trip' _ _ (-1) = []
+trip' fw st rem = next : trip' fw next (rem - 1)
+    where next = step fw st
 
-trip :: Firewall -> State -> Int -> [(Int, State)]
-trip _ _ (-1) = []
-trip fw st rem = next : trip fw st' (rem - 1)
-    where next@(_, st') = (step fw st)
+findDelay :: Firewall -> State -> Int
+findDelay fw st = findDelay' fw st 0
+
+findDelay' :: Firewall -> State -> Int -> Int
+findDelay' fw st delta =
+    let trip' = trip fw st
+        detected = getAny . foldMap (\(_, (det, _), _) -> Any $ det) $ trip'
+        (_, _, scanners') = head trip'
+    in  if not detected then delta else findDelay' fw (-1, undetected, scanners') (delta + 1)
