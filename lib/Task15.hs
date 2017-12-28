b732f1d9bdf6b4b3f344f1d062020f553bc20134
Pietro Lorefice <pietro.lorefice@gmail.com>
2017-12-28 14:08:12 +0100

    Task 15: second subtask implementation

--- lib/Task15.hs
+++ lib/Task15.hs
@@ -1,19 +1,25 @@
 module Task15
 ( Generator
 , mkGen
-, next
 , nexts
+, nexts'
 ) where
 
-data Generator = Generator Int Int
+data Generator = Generator Int Int Int
 
-mkGen :: Int -> Int -> Generator
-mkGen f n = Generator f n
+mkGen :: Int -> Int -> Int -> Generator
+mkGen f m n = Generator f m n
 
 next :: Generator -> (Int, Generator)
-next (Generator f n) = (k, Generator f k)
+next (Generator f m n) = (k, Generator f m k)
     where k = (n * f) `mod` 2147483647
 
 nexts :: Generator -> [Int]
 nexts g = n' : nexts g'
+    where (n', g') = next g
+
+nexts' :: Generator -> [Int]
+nexts' g@(Generator f m n)
+    | n' `mod` m == 0 = n' : nexts' g'
+    | otherwise       = nexts' g'
     where (n', g') = next g
--- task15/Main.hs
+++ task15/Main.hs
@@ -5,11 +5,21 @@ import Task15
 import Data.Bits
 
 genA :: Generator
-genA = mkGen 16807 512
+genA = mkGen 16807 4 512
 
 genB :: Generator
-genB = mkGen 48271 191
+genB = mkGen 48271 8 191
 
 main :: IO ()
-main = print . length . filter matching . take 40000000 $ zip (nexts genA) (nexts genB)
-    where matching (a,b) = (a .&. 0xFFFF) == (b .&. 0xFFFF)
+main = do
+    putStrLn . ("15a: " ++) . show $ taskA
+    putStrLn . ("15b: " ++) . show $ taskB
+
+taskA :: Int
+taskA = length . filter matching . take 40000000 $ zip (nexts genA) (nexts genB)
+
+taskB :: Int
+taskB = length . filter matching . take 5000000 $ zip (nexts' genA) (nexts' genB)
+
+matching :: (Int, Int) -> Bool
+matching (a,b) = (a .&. 0xFFFF) == (b .&. 0xFFFF)
